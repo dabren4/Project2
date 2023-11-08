ac18d7b574b24259532eddc606c7749da635ac24
darren <darrenxychen@gmail.com>
2023-11-07 21:25:56 -0800

    copied the parser to dynamic.py

---
+++ dynamic.py
@@ -0,0 +1,28 @@
+# Darren Chen
+# CPSC335
+
+import ast
+def parse_file(file_path):
+  with open(file_path, 'r') as file:
+    lines = [line.strip() for line in file if line.strip()]
+
+  parsed_results = []
+
+  i = 0
+  while i < len(lines):
+    N = int(lines[i])
+    i += 1
+
+    stock_and_values = ast.literal_eval(lines[i].strip())
+    i += 1
+    amount = int(lines[i].strip())
+    i += 1
+
+    parsed_results.append((N, stock_and_values, amount))
+    # make a tuple
+
+  return parsed_results
+
+file_path = 'input.txt'
+
+parsed_inputs = parse_file(file_path)
